39ce917e8d60dc43c3fa28ca03cb206e33966ddd
xagusramirezx <47385867+xagusramirezx@users.noreply.github.com>
2020-02-05 10:18:26 -0300

    ejercicio 7 solo suma
    los demas los copio

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,66 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+
+	primerNumero=parseInt(document.getElementById('numeroUno').value);
+	segundoNumero=parseInt(document.getElementById('numeroDos').value);
+	
+	resultado=primerNumero+segundoNumero;
+
+	console.log("La suma de ambos numeros es: " + resultado);
 	
 }
 
 function restar()
 {
 	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+
+	primerNumero=parseInt(document.getElementById('numeroUno').value);
+	segundoNumero=parseInt(document.getElementById('numeroDos').value);
+	
+	resultado=primerNumero-segundoNumero;
+
+	console.log("La diferencia de ambos numeros es: " + resultado);
+	
 }
 
 function multiplicar()
 { 
 	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+	primerNumero=parseInt(document.getElementById('numeroUno').value);
+	segundoNumero=parseInt(document.getElementById('numeroDos').value);
+	
+	resultado=primerNumero*segundoNumero;
+
+	console.log("El producto de ambos numeros es: " + resultado);
 }
 
 function dividir()
 {
+	
+	var primerNumero;
+	var segundoNumero;
+	var resultado;
+
+
+	primerNumero=parseInt(document.getElementById('numeroUno').value);
+	segundoNumero=parseInt(document.getElementById('numeroDos').value);
+	
+	resultado=primerNumero/segundoNumero;
+
+	console.log("La divicion de ambos numeros es: " + resultado);
 	
 }
 
